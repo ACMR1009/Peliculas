9cf2c85eff13f6782d165269f99235f05c4c91bc
ACMR1009 <anacristina.munguia@gmail.com>
2019-02-20 08:39:48 -0700

    Versión Corregida

--- app/src/main/java/com/example/peliculas/MainActivity.java
+++ app/src/main/java/com/example/peliculas/MainActivity.java
@@ -17,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitulo1 = (TextView) findViewById(R.id.textView);
+        txtDescripcion1 = (TextView) findViewById(R.id.textView2);
+        txtTitulo2 = (TextView) findViewById(R.id.textView3);
+        txtDescripcion2 = (TextView) findViewById(R.id.textView4);
+        txtTitulo3 = (TextView) findViewById(R.id.textView5);
+        txtDescripcion3 = (TextView) findViewById(R.id.textView6);
+
+        txtTitulo1.setText("How To Train Your Dragos: The Hidden World");
+        txtDescripcion1.setText("Cuando Hipo descubre que Chimuelo no es el único furia nocturna, debe ir en busca del mundo oculto.");
+        txtTitulo2.setText("The Lego Movie: The Second Part");
+        txtDescripcion2.setText("Han pasado 5 años y ahora los ciudadanos enfrentan una nueva amenaza: Invasores Lego DUPLO® del espacio exterior.");
+        txtTitulo3.setText("Green Book");
+        txtDescripcion3.setText("Un pianista negro contrata a un italoamericano para que sea su chofer y guardaespaldas durante una gira por el sur de los EUA en 1962.");
+
     }
 }
